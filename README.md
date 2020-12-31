1e3b9fb98c2e4c8654b1c602f6c7d37d5c3c3114
AndreVc123 <59818468+AndreVc123@users.noreply.github.com>
2020-12-31 00:08:51 -0600

    Primer commit - rest server

---
+++ README.md
@@ -0,0 +1,19 @@
+
+
+#SOCKET-SERVER con Typescript
+
+Reconstruir modulos de node
+```
+npm install
+```
+
+Generar el DIST
+```
+tsc -w
+```
+
+Levantar servidor
+```
+nodemon dist/
+    o
+node dist/
---
+++ routes/router.ts
@@ -0,0 +1,43 @@
+import { Router, Request, Response} from 'express';
+
+const router = Router();
+
+router.get('/mensajes', (req: Request, res: Response) => {
+
+    res.json({
+        ok: true,
+        mensaje: 'todo esta bien'
+    })
+})
+
+router.post('/mensajes', (req, res) => {
+
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+
+    res.json({
+        ok: true,
+        mensaje: 'Mensaje post listo',
+        cuerpo,
+        de
+    })
+})
+
+
+router.post('/mensajes/:id', (req, res) => {
+
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+    const id = req.params.id;
+
+    res.json({
+        ok: true,
+        mensaje: 'Mensaje post listo',
+        cuerpo,
+        de,
+        id
+    })
+})
+
+
+export default router;
